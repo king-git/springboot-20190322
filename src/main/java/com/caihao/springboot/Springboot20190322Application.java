8002bf6b222790ff045f73967680beca16c23982
caihao <kingch92@163.com>
2019-03-22 21:37:19 +0800

    Initial commit

---
+++ src/main/java/com/caihao/springboot/Springboot20190322Application.java
@@ -0,0 +1,13 @@
+package com.caihao.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot20190322Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot20190322Application.class, args);
+    }
+
+}
