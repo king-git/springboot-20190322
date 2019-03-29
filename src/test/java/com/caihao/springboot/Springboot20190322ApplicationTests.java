e38aca8e61535dffe91581883bc4c9031add09c3
caihao <kingch92@163.com>
2019-03-29 18:33:36 +0800

    新增测试

--- src/test/java/com/caihao/springboot/Springboot20190322ApplicationTests.java
+++ src/test/java/com/caihao/springboot/Springboot20190322ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.caihao.springboot;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Springboot20190322ApplicationTests {
 
+
+
     @Test
     public void contextLoads() {
+        System.err.println("测试日志");
+        Log log = LogFactory.getLog(getClass());
+
+//      日志级别从低到高
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+
+
     }
 
 }
