eec1c140527d0ccd4bf3a7dcdeb2de4b7601624f
caihao <kingch92@163.com>
2019-04-04 18:37:39 +0800

    新增测试

--- src/main/java/com/caihao/springboot/controller/TestController.java
+++ src/main/java/com/caihao/springboot/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 /**
  * @author CaiHao
  * @create 2019-03-26 下午 19:44:20
@@ -39,4 +41,15 @@ public class TestController {
         return "caihao";
 
     }
+
+    @RequestMapping("/tm")
+    public String maptest(Map<String,Object> map){
+
+        map.put("name","蔡浩");
+        map.put("gender","男");
+
+        return "mapTest";
+    }
+
+
 }
